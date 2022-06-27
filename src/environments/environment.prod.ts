844b1103745911db75abaa166502ff75c348e7a3
Shri nivas <shri.nivas@troology.com>
2022-06-27 17:13:42 +0530

    git push

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,8 @@
 export const environment = {
   production: true,
-  // serverUrl: 'https://sidm.evalue8.info/',
- serverUrl: 'http://localhost:3000/',
-// serverUrl: 'https://railway-la.evalue8.info/',
-// download: 'https://sidm.evalue8.info/uploads/'
-//  download: 'http://localhost:3000/uploads/'
+
+//  serverUrl: 'http://localhost:3000/',
+serverUrl: 'https://railway-la.evalue8.info/',
+
  download: 'https://railway-la.evalue8.info/uploads/'
 };
