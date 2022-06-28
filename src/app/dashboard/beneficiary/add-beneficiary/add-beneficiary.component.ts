d77fcabae50c399c55c70c81cc48c47e6bfe9623
Shri nivas <shri.nivas@troology.com>
2022-06-28 14:24:02 +0530

    git push

--- src/app/dashboard/beneficiary/add-beneficiary/add-beneficiary.component.ts
+++ src/app/dashboard/beneficiary/add-beneficiary/add-beneficiary.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Beneficiary } from 'src/app/shared/data.model';
 import { DataService } from 'src/app/shared/data.service';
+import { HttpsService } from 'src/app/shared/https.service';
 import { ToasterService } from 'src/app/shared/toaster.service';
 
 @Component({
@@ -12,16 +13,34 @@ import { ToasterService } from 'src/app/shared/toaster.service';
 })
 export class AddBeneficiaryComponent implements OnInit {
   beneficiary:FormGroup;
-
-  
+state:any=[]
+district:any=[]
+  project:any
   constructor( private fb:FormBuilder,
     private router: Router,
     private data: DataService,
-    private toster: ToasterService){
+    private httpService:HttpsService,) {
+      this.httpService.getProject().subscribe((data:any)=>{
+        this.project=data.projects
+        this.project.map((item:any)=>{
+          item.projectDetails.map((projectData:any)=>{
+            if (!this.state.includes(projectData.state)) {
+              // ✅ only runs if value not in array
+              this.state.push(projectData.state);
+            }
+          })
+
+
+
+        })
+
+      })
       this.beneficiary=this.fb.group({
+        projectName:[''],
+        projectID:[''],
         date:[''],
-        referenceNo:[''],
-        user:[''],
+        state:[''],
+        district:[''],
         beneficiary:[''],
         beneficiaryName:[''],
         gataNumber:[''],
@@ -33,29 +52,75 @@ export class AddBeneficiaryComponent implements OnInit {
         registrationAmount:[''],
         remark:['']
       })
+
+
     }
 
   ngOnInit(): void {
   }
-  
-  onSubmit() {console.log(this.beneficiary);
+
+  getPriject(event:any){
+
+this.state=[]
+this.project.map((item:any)=>{
+  if(item.projectName===event.target.value){
+    this.beneficiary.get('projectID')?.setValue(item.projectID)
+    this.beneficiary.get('projectID')?.updateValueAndValidity
+  item.projectDetails.map((projectData:any)=>{
+    if (!this.state.includes(projectData.state)) {
+      // ✅ only runs if value not in array
+      this.state.push(projectData.state);
+    }
+  })}
+  this.beneficiary.get('state')?.reset()
+  this.beneficiary.get('state')?.updateValueAndValidity
+
+
+
+})
+
+
+  }
+
+  getDistrict(event:any){
+this.district=[]
+    this.project.map((item:any)=>{
+      if(item.projectName===this.beneficiary.value.projectName){
+      item.projectDetails.map((projectData:any)=>{
+        if (projectData.state===event.target.value) {
+          if(!this.district.includes(projectData.district))
+
+
+          // ✅ only runs if value not in array
+          this.district.push(projectData.district);
+        }
+      })}
+
+
+
+    })
+
+
+  }
+
+  onSubmit() {
     // let date=this.beneficiary.value.data.toString()
     const beneficiary=[new Beneficiary(
       this.beneficiary.value.date,
       this.beneficiary.value.beneficiaryName,
        this.beneficiary.value.gataNumber,
-        this.beneficiary.value.rakba, 
+        this.beneficiary.value.rakba,
         this.beneficiary.value.pratifalRate,
         this.beneficiary.value.beneficaryShare,
-         this.beneficiary.value.chequeNumber, 
-         this.beneficiary.value.chequeDate, 
+         this.beneficiary.value.chequeNumber,
+         this.beneficiary.value.chequeDate,
          this.beneficiary.value.registrationAmount)]
-         
 
-         
+
+
          this.data.addBeneficiary(beneficiary)
          this.router.navigate(['/dashboard/beneficiary/beneficiariesList'])
-  
+
   }
 
   cancel() {
