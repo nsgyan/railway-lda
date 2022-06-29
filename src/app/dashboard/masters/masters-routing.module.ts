5212873f0e95f3cd228eea1a055b39d71ea7757e
Shri nivas <shri.nivas@troology.com>
2022-06-29 12:25:36 +0530

    git push

--- src/app/dashboard/masters/masters-routing.module.ts
+++ src/app/dashboard/masters/masters-routing.module.ts
@@ -2,20 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BankComponent } from './bank/bank.component';
 import { BlockMasterEditComponent } from './block-master-edit/block-master-edit.component';
-import { BlockMasterListComponent } from './block-master-list/block-master-list.component';
+// import { BlockMasterListComponent } from './block-master-list/block-master-list.component';
 import { BlockMasterComponent } from './block-master/block-master.component';
 import { DistictMasterComponent } from './distict-master/distict-master.component';
 import { DistricMasterEditComponent } from './distric-master-edit/distric-master-edit.component';
-import { DistrictMasterListComponent } from './district-master-list/district-master-list.component';
+// import { DistrictMasterListComponent } from './district-master-list/district-master-list.component';
 import { LandCategoryComponent } from './land-category/land-category.component';
 import { LandNatureComponent } from './land-nature/land-nature.component';
 import { LandTypeComponent } from './land-type/land-type.component';
 import { ObjectionComponent } from './objection/objection.component';
 import { StateMasterEditComponent } from './state-master-edit/state-master-edit.component';
-import { StateMasterListComponent } from './state-master-list/state-master-list.component';
+// import { StateMasterListComponent } from './state-master-list/state-master-list.component';
 import { StateMasterComponent } from './state-master/state-master.component';
 import { VillageMasterEditComponent } from './village-master-edit/village-master-edit.component';
-import { VillageMasterListComponent } from './village-master-list/village-master-list.component';
+// import { VillageMasterListComponent } from './village-master-list/village-master-list.component';
 import { VillageMasterComponent } from './village-master/village-master.component';
 
 const routes: Routes = [
--- src/app/dashboard/masters/masters.module.ts
+++ src/app/dashboard/masters/masters.module.ts
@@ -7,10 +7,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DistictMasterComponent } from './distict-master/distict-master.component';
 import { BlockMasterComponent } from './block-master/block-master.component';
 import { VillageMasterComponent } from './village-master/village-master.component';
-import { BlockMasterListComponent } from './block-master-list/block-master-list.component';
-import { VillageMasterListComponent } from './village-master-list/village-master-list.component';
-import { StateMasterListComponent } from './state-master-list/state-master-list.component';
-import { DistrictMasterListComponent } from './district-master-list/district-master-list.component';
+
 import { BlockMasterEditComponent } from './block-master-edit/block-master-edit.component';
 import { StateMasterEditComponent } from './state-master-edit/state-master-edit.component';
 import { DistricMasterEditComponent } from './distric-master-edit/distric-master-edit.component';
@@ -28,10 +25,6 @@ import { ObjectionComponent } from './objection/objection.component';
     DistictMasterComponent,
     BlockMasterComponent,
     VillageMasterComponent,
-    BlockMasterListComponent,
-    VillageMasterListComponent,
-    StateMasterListComponent,
-    DistrictMasterListComponent,
     BlockMasterEditComponent,
     StateMasterEditComponent,
     DistricMasterEditComponent,
