d637f5308bf0ca788a7ff385d9e4b7be86f43f39
Shri nivas <shri.nivas@troology.com>
2022-07-08 12:17:59 +0530

    git push

---
+++ src/app/dashboard/serway/serway.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SerwayRoutingModule } from './serway-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SerwayRoutingModule
+  ]
+})
+export class SerwayModule { }
