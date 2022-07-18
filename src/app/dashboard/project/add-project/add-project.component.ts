6642ff159eb7cbf953c289e9f2eb02e77c8ddb52
Shri nivas <shri.nivas@troology.com>
2022-07-18 10:49:01 +0530

    git push

--- src/app/dashboard/project/add-project/add-project.component.ts
+++ src/app/dashboard/project/add-project/add-project.component.ts
@@ -156,9 +156,10 @@ control.at(i).get(type)?.updateValueAndValidity
     evt = (evt) ? evt : event;
     const charCode = (evt.charCode) ? evt.charCode : ((evt.keyCode) ? evt.keyCode :
       ((evt.which) ? evt.which : 0));
-    if (charCode > 31 && (charCode < 65 || charCode > 90) &&
-      (charCode < 97 || charCode > 122)) {
-      return false;
+    if (charCode < 31 && (charCode > 65 || charCode < 90) &&
+      (charCode > 97 || charCode < 122)) {
+        return true;
+
     }
     return true;
   }
